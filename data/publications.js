f0115ed83ae3e890c17847d2bf4b84f317f22f06
Angkon-Dutta-Joy <125572441+Angkon-Dutta-Joy@users.noreply.github.com>
2026-08-27 23:00:26 +0600

    Update publications.js

--- data/publications.js
+++ data/publications.js
@@ -1,6 +1,6 @@
 const PUBLICATIONS = [
   {
-    authors: "<strong>Angkon Dutta Joy</strong>, et al.",
+    authors: "<strong>Himika Tasnim; Angkon Dutta Joy; Anindita Dutta; Rawhatur Rabbi; Aniqua Nusrat Zereen</strong>",
     title: "Temporal Fusion of Convolutional and LSTM Networks for Vision-Based Fall Detection Using Anatomical Keypoints",
     venue: "International Conference on Computer and Information Technology (ICCIT), 2025",
     links: [
